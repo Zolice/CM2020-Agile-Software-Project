ec7a2260ef6a54531f2f6a0c784987f83b285312
Yong Zhen Long <vividzhenlong@gmail.com>
2024-07-15 20:01:05 +0800

    Initial Commit

---
+++ app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <Hero title="CM2020-Agile-Software-Project" msg="Demonstration of DaisyUI components working with TailwindCSS"/>
+    <NuxtWelcome />
+  </div>
+</template>
