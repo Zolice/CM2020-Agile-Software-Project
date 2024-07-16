66f96ba7c3ee549280004fa9818280f808009feb
M-A-C255 <zaypainglatt04@gmail.com>
2024-07-16 14:33:50 +0800

    applied prettier format on the whole codebase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
-  }
-})
+  },
+});
