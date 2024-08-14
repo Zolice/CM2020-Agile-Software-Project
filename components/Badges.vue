2f8ef261ed685c26c41e787a6314e748bdcca02d
M-A-C255 <zaypainglatt04@gmail.com>
2024-08-14 23:58:03 +0800

    removed inset-0 due to nested modal bug

--- components/Badges.vue
+++ components/Badges.vue
@@ -1,12 +1,12 @@
 <template>
-  <div v-for="badge in badges" :key="badge.id" class="border">
+  <div v-for="badge in badges" :key="badge.id">
     <img
       :src="badge.image"
       :alt="badge.name"
       class="w-16 h-16 rounded transition-transform transform group-hover:scale-110"
     />
     <div
-      class="absolute inset-0 flex items-center justify-center bg-black bg-opacity-75 text-white text-xs rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300 scale-110"
+      class="absolute flex items-center justify-center bg-black bg-opacity-75 text-white text-xs rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300 scale-110"
     >
       {{ badge.name }}
     </div>
