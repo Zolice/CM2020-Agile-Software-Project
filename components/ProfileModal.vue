9282ee43f579af5fad76994673cab7d3b0ade4ae
M-A-C255 <zaypainglatt04@gmail.com>
2024-08-14 22:14:26 +0800

    converted name tag into a component

--- components/ProfileModal.vue
+++ components/ProfileModal.vue
@@ -121,23 +121,7 @@
               </div>
 
               <!-- Name tags -->
-              <!-- TODO: Considering making this a component -->
-              <div class="mt-4">
-                <h4 class="text-xl font-bold">Name Tags</h4>
-                <div class="flex flex-wrap gap-2">
-                  <div
-                    v-for="nametag in nametags"
-                    :key="nametag.id"
-                    class="border"
-                  >
-                    <img
-                      :src="nametag.image"
-                      :alt="nametag.name"
-                      class="w-40 h-10"
-                    />
-                  </div>
-                </div>
-              </div>
+              <NameTags :nametags="nametags" />
             </div>
 
             <!-- Borders -->
