4552b6f14bf6a60b1e31ffee75392efa2eb393ff
M-A-C255 <zaypainglatt04@gmail.com>
2024-08-14 22:15:28 +0800

    converted borders into a component

--- components/ProfileModal.vue
+++ components/ProfileModal.vue
@@ -125,25 +125,7 @@
             </div>
 
             <!-- Borders -->
-            <!-- TODO: Considering making this a component -->
-            <div class="main-four col w-1/2">
-              <div class="mt-4">
-                <h4 class="text-xl font-bold">Borders</h4>
-                <div class="flex flex-wrap gap-2 mt-4">
-                  <div
-                    v-for="border in borders"
-                    :key="border.id"
-                    class="border"
-                  >
-                    <img
-                      :src="border.image"
-                      :alt="border.name"
-                      class="w-16 h-16"
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
+            <Borders :borders="borders" />
           </div>
         </div>
       </div>
