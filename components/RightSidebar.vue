6f8bc6fce048e644b197435e4d865244edba1189
Yong Zhen Long <vividzhenlong@gmail.com>
2024-08-26 17:36:34 +0800

    Added Task Display component for Timeline and Task List

--- components/RightSidebar.vue
+++ components/RightSidebar.vue
@@ -2,7 +2,7 @@
   <!-- Sidebar -->
   <div
     class="transition-all duration-150 bg-base-300 block fixed md:relative h-screen z-10"
-    :class="[isSidebarOpen ? 'md:w-[16rem] w-10/12' : 'w-0 collapse']"
+    :class="[isSidebarOpen ? 'md:w-[18rem] w-10/12' : 'w-0 collapse']"
   >
     <div class="h-full bg-base-300 w-full flex flex-col align-center">
       <div class="flex justify-between items-center px-4 py-3 h-min">
@@ -10,8 +10,8 @@
           class="w-fit text-center text-2xl focus:outline-none h-fit"
           @click="toggleSidebar"
         >
-          <span v-if="isSidebarOpen"
-            ><svg
+          <span v-if="isSidebarOpen">
+            <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
@@ -23,20 +23,51 @@
                 stroke-linecap="round"
                 stroke-linejoin="round"
                 d="m8.25 4.5 7.5 7.5-7.5 7.5"
-              /></svg
-          ></span>
+              />
+            </svg>
+          </span>
         </button>
-        <p v-if="isSidebarOpen" class="text-center h-fit">Dropdown</p>
+        <select
+          v-if="isSidebarOpen"
+          v-model="sidebarType"
+          class="select select-sm"
+        >
+          <option>Task List</option>
+          <option>Timeline</option>
+        </select>
       </div>
       <nav
         v-if="isSidebarOpen"
         class="flex-1 p-4 flex flex-col justify-between"
       >
         <!-- Sidebar content here -->
-        <div class="flex flex-col gap-4 h-fit overflow-auto">
-          <span>Today</span>
-          <span>Tomorrow</span>
-          <span>Other stuff</span>
+        <div v-if="sidebarType=='Task List'" class="flex flex-col gap-2 h-fit overflow-auto">
+          <span class="text-lg">Overdue</span>
+        </div>
+        <div v-else class="flex flex-col gap-2 h-fit overflow-auto">
+          <div class="flex flex-col gap-1">
+            <span class="text-lg">Overdue</span>
+            <TaskDisplay
+              name="Assignment Due -- Algorithms and Data Structures II"
+              due-date="Today"
+              colour="red"
+              description="Algorithms and Data Structures II -- Week 6 https://www.coursera.org/learn/uol-algorithms-and-data-structures-2/home/week/6You have 1 item due - 3.403 Topic 3 summative assessment [001]Stay on track by completing your assignment now!"
+            />
+            <TaskDisplay
+              name="Assignment Due -- Algorithms and Data Structures II"
+              due-date="Today"
+              colour="yellow"
+              description="Algorithms and Data Structures II -- Week 6 https://www.coursera.org/learn/uol-algorithms-and-data-structures-2/home/week/6You have 1 item due - 3.403 Topic 3 summative assessment [001]Stay on track by completing your assignment now!"
+            />
+            <TaskDisplay
+              name="Assignment Due -- Algorithms and Data Structures II"
+              due-date="Today"
+              colour="green"
+              description="Algorithms and Data Structures II -- Week 6 https://www.coursera.org/learn/uol-algorithms-and-data-structures-2/home/week/6You have 1 item due - 3.403 Topic 3 summative assessment [001]Stay on track by completing your assignment now!"
+            />
+          </div>
+          <span class="text-lg">Today's Tasks</span>
+          <span class="text-lg">Upcoming</span>
         </div>
       </nav>
     </div>
@@ -48,9 +79,6 @@ defineProps({
   toggleSidebar: Function,
   isSidebarOpen: Boolean,
 });
+
+const sidebarType = ref("Timeline");
 </script>
-<style scoped>
-.title {
-  font-family: "Inter";
-}
-</style>
---
+++ components/TaskDisplay.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- cursor-pointer hover:bg-base-100 -->
+  <div class="flex flex-col btn btn-ghost h-fit items-start p-1 gap-1">
+    <span class="line-clamp-1 text-start">{{ name }}</span>
+    <!-- <span class="text-sm">{{ dueDate }}</span> -->
+    <span
+      class="badge badge-ghost badge-sm font-normal"
+      :class="[
+        colour == 'red' ? 'border-none bg-[#EA9999] text-black' : '',
+        colour == 'yellow' ? 'border-none bg-[#FCE8B2] text-black' : '',
+        colour == 'green' ? 'border-none bg-[#BDD9CD] text-black' : '',
+      ]"
+      >{{ dueDate }}</span
+    >
+    <span class="text-xs line-clamp-2 text-start font-normal">{{
+      description
+    }}</span>
+  </div>
+</template>
+
+<script setup lang="jsx">
+defineProps({
+  name: String,
+  colour: String,
+  dueDate: String,
+  description: String,
+});
+</script>
