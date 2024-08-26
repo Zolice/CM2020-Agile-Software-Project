c18d2c14ce9b643bf8155d7dcd114e2fc8a6dcbc
Yong Zhen Long <vividzhenlong@gmail.com>
2024-08-26 19:53:46 +0800

    Added Task List View component Added support for displaying tasks in Task List. Lengthened the Right Sidebar slightly

--- components/RightSidebar.vue
+++ components/RightSidebar.vue
@@ -2,7 +2,7 @@
   <!-- Sidebar -->
   <div
     class="transition-all duration-150 bg-base-300 block fixed md:relative h-screen z-10"
-    :class="[isSidebarOpen ? 'md:w-[18rem] w-10/12' : 'w-0 collapse']"
+    :class="[isSidebarOpen ? 'md:w-[20rem] w-10/12' : 'w-0 collapse']"
   >
     <div class="h-full bg-base-300 w-full flex flex-col align-center">
       <div class="flex justify-between items-center px-4 py-3 h-min">
@@ -38,36 +38,12 @@
       </div>
       <nav
         v-if="isSidebarOpen"
-        class="flex-1 p-4 flex flex-col justify-between"
+        class="flex-1 p-4 flex flex-col justify-between overflow-auto"
       >
         <!-- Sidebar content here -->
-        <div v-if="sidebarType=='Task List'" class="flex flex-col gap-2 h-fit overflow-auto">
-          <span class="text-lg">Overdue</span>
-        </div>
+        <TaskListView v-if="sidebarType == 'Task List'" />
         <div v-else class="flex flex-col gap-2 h-fit overflow-auto">
-          <div class="flex flex-col gap-1">
-            <span class="text-lg">Overdue</span>
-            <TaskDisplay
-              name="Assignment Due -- Algorithms and Data Structures II"
-              due-date="Today"
-              colour="red"
-              description="Algorithms and Data Structures II -- Week 6 https://www.coursera.org/learn/uol-algorithms-and-data-structures-2/home/week/6You have 1 item due - 3.403 Topic 3 summative assessment [001]Stay on track by completing your assignment now!"
-            />
-            <TaskDisplay
-              name="Assignment Due -- Algorithms and Data Structures II"
-              due-date="Today"
-              colour="yellow"
-              description="Algorithms and Data Structures II -- Week 6 https://www.coursera.org/learn/uol-algorithms-and-data-structures-2/home/week/6You have 1 item due - 3.403 Topic 3 summative assessment [001]Stay on track by completing your assignment now!"
-            />
-            <TaskDisplay
-              name="Assignment Due -- Algorithms and Data Structures II"
-              due-date="Today"
-              colour="green"
-              description="Algorithms and Data Structures II -- Week 6 https://www.coursera.org/learn/uol-algorithms-and-data-structures-2/home/week/6You have 1 item due - 3.403 Topic 3 summative assessment [001]Stay on track by completing your assignment now!"
-            />
-          </div>
-          <span class="text-lg">Today's Tasks</span>
-          <span class="text-lg">Upcoming</span>
+          <span class="text-lg">Overdue</span>
         </div>
       </nav>
     </div>
@@ -75,10 +51,12 @@
 </template>
 
 <script setup lang="jsx">
+// Import
 defineProps({
   toggleSidebar: Function,
   isSidebarOpen: Boolean,
 });
 
+// Settings
 const sidebarType = ref("Timeline");
 </script>
--- components/TaskDisplay.vue
+++ components/TaskDisplay.vue
@@ -2,19 +2,19 @@
   <!-- cursor-pointer hover:bg-base-100 -->
   <div class="flex flex-col btn btn-ghost h-fit items-start p-1 gap-1">
     <span class="line-clamp-1 text-start">{{ name }}</span>
-    <!-- <span class="text-sm">{{ dueDate }}</span> -->
     <span
-      class="badge badge-ghost badge-sm font-normal"
+      class="badge badge-ghost badge-sm font-normal border-none"
       :class="[
-        colour == 'red' ? 'border-none bg-[#EA9999] text-black' : '',
-        colour == 'yellow' ? 'border-none bg-[#FCE8B2] text-black' : '',
-        colour == 'green' ? 'border-none bg-[#BDD9CD] text-black' : '',
+        colour == 'red' ? 'bg-[#EA9999] text-black' : '',
+        colour == 'yellow' ? 'bg-[#FCE8B2] text-black' : '',
+        colour == 'green' ? 'bg-[#BDD9CD] text-black' : '',
       ]"
-      >{{ dueDate }}</span
     >
-    <span class="text-xs line-clamp-2 text-start font-normal">{{
-      description
-    }}</span>
+      Due on {{ dueDate }}
+    </span>
+    <span class="text-xs line-clamp-2 hover:line-clamp-6 text-start font-normal">
+      {{ description }}
+    </span>
   </div>
 </template>
 
@@ -24,5 +24,6 @@ defineProps({
   colour: String,
   dueDate: String,
   description: String,
+  uid: String,
 });
 </script>
