5933421e99edb77e733dfd632902d839cb41e9ad
M-A-C255 <zaypainglatt04@gmail.com>
2024-08-27 13:36:19 +0800

    added DailyView component

--- pages/index.vue
+++ pages/index.vue
@@ -16,6 +16,32 @@ var months = [
   "DECEMBER",
 ];
 var dayNames = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+const hours = [
+  "12:00 AM",
+  "01:00 AM",
+  "02:00 AM",
+  "03:00 AM",
+  "04:00 AM",
+  "05:00 AM",
+  "06:00 AM",
+  "07:00 AM",
+  "08:00 AM",
+  "09:00 AM",
+  "10:00 AM",
+  "11:00 AM",
+  "12:00 PM",
+  "01:00 PM",
+  "02:00 PM",
+  "03:00 PM",
+  "04:00 PM",
+  "05:00 PM",
+  "06:00 PM",
+  "07:00 PM",
+  "08:00 PM",
+  "09:00 PM",
+  "10:00 PM",
+  "11:00 PM",
+];
 var dates = ref([]);
 
 var previousDates = ref([]);
@@ -157,10 +183,7 @@ function nextMonth() {
   <div v-else-if="calendarViewType === 'Weekly'">Supposed to be Weekly</div>
 
   <!-- Render Daily view -->
-  <!-- TODO: Replace with daily view component -->
-  <div v-else-if="calendarViewType === 'Daily'">
-    Supposed to be Daily View stuff
-  </div>
+  <DailyView v-else-if="calendarViewType === 'Daily'" :hours="hours" />
 
   <!-- Test for Listing tasks - to remove later -->
   <div
